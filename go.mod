e2688fead4ee046bec2888fd9eb45e9ce62123da
Bluesaxorcist <joshua@operatorfoundation.org>
2020-04-08 11:44:46 -0500

    added .sh tests and json files

--- go.mod
+++ go.mod
@@ -16,8 +16,8 @@ require (
 	github.com/mufti1/interconv v0.0.0-20190313040840-d7c72925c656
 	github.com/shadowsocks/go-shadowsocks2 v0.1.0 // indirect
 	github.com/willscott/goturn v0.0.0-20170802220503-19f41278d0c9
-	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6 // indirect
-	golang.org/x/net v0.0.0-20200301022130-244492dfa37a
-	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+	golang.org/x/sys v0.0.0-20200406155108-e3b113bbe6a4 // indirect
 	golang.org/x/text v0.3.2 // indirect
 )
---
+++ testDust.sh
@@ -0,0 +1,30 @@
+# This script runs a full end-to-end functional test of the dispatcher and the Replicant transport, using two netcat instances as the application server and application client.
+# An alternative way to run this test is to run each command in its own terminal. Each netcat instance can be used to type content which should appear in the other.
+
+# Update and build code
+go get -u github.com/OperatorFoundation/shapeshifter-dispatcher
+
+# Run a demo application server with netcat
+nc -l 3333 &
+
+# Run the transport server
+./shapeshifter-dispatcher -transparent -server -state state -orport 127.0.0.1:3333 -transports dust -bindaddr dust-127.0.0.1:2222 -optionsFile dustServer.json -logLevel DEBUG -enableLogging &
+
+sleep 1
+
+# Run the transport client
+./shapeshifter-dispatcher -transparent -client -state state -target 127.0.0.1:2222 -transports dust -proxylistenaddr 127.0.0.1:1443 -optionsFile dustClient.json -logLevel DEBUG -enableLogging &
+
+sleep 1
+
+# Run a demo application client with netcat
+echo "Test successful!" | nc localhost 1443 &
+
+sleep 1
+
+echo "Testing complete. Killing processes."
+
+killall shapeshifter-dispatcher
+killall nc
+
+echo "Done."
