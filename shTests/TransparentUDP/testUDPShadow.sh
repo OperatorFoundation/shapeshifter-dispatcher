fcba8e0f5c18b2927dd49fa8f36fc92cc5355561
Bluesaxorcist <joshua@operatorfoundation.org>
2021-04-07 18:41:45 -0500

    fixed shadow Transparent UDP test

--- shTests/TransparentUDP/testUDPShadow.sh
+++ shTests/TransparentUDP/testUDPShadow.sh
@@ -16,7 +16,7 @@ nc -l -u 3333 >$FILENAME &
 sleep 1
 
 # Run the transport client
-~/go/bin/shapeshifter-dispatcher -transparent -udp -client -state state -target 127.0.0.1:2222 -transports shadow -proxylistenaddr 127.0.0.1:1443 -optionsFile ../../ConfigFiles/shadowClient.json -logLevel DEBUG -enableLogging &
+~/go/bin/shapeshifter-dispatcher -transparent -udp -client -state state -target 127.0.0.1:2222 -transports shadow -proxylistenaddr 127.0.0.1:1443 -optionsFile ../../ConfigFiles/shadowClientChaCha.json -logLevel DEBUG -enableLogging &
 
 sleep 1
 
